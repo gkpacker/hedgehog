3e43639b7d9d946255fbd97049c2d75d89eea0be
Gabriel <ggkpacker@gmail.com>
2021-01-21 19:08:23 -0300

    Create trader supervision tree

--- apps/naive/lib/naive.ex
+++ apps/naive/lib/naive.ex
@@ -1,6 +1,16 @@
 defmodule Naive do
   alias Streamer.Binance.TradeEvent
 
+  def start_trading(symbol) do
+    symbol = String.upcase(symbol)
+
+    {:ok, _pid} =
+      DynamicSupervisor.start_child(
+        Naive.DynamicSymbolSupervisor,
+        {Naive.SymbolSupervisor, symbol}
+      )
+  end
+
   def send_event(%TradeEvent{} = event) do
     GenServer.cast(:trader, event)
   end
