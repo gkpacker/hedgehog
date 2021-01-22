078078d71ed9f1420f08b7b4b5e6d6874645f895
Gabriel <ggkpacker@gmail.com>
2021-01-22 11:38:40 -0300

    Allow traders to trigger rebuy orders

--- apps/naive/lib/naive/leader.ex
+++ apps/naive/lib/naive/leader.ex
@@ -1,11 +1,11 @@
 defmodule Naive.Leader do
   use GenServer
 
+  alias Decimal, as: D
   alias Naive.Trader
 
   require Logger
 
-  @filter_type "PRICE_FILTER"
   @binance_client Application.get_env(:naive, :binance_client)
 
   defmodule State do
@@ -16,13 +16,6 @@ defmodule Naive.Leader do
 
   defmodule TraderData do
     defstruct pid: nil, ref: nil, state: nil
-  end
-
-  def notify(:trader_state_updated, state) do
-    GenServer.call(
-      :"#{__MODULE__}-#{state.symbol}",
-      {:update_trader_state, state}
-    )
   end
 
   def start_link(symbol) do
@@ -41,16 +34,24 @@ defmodule Naive.Leader do
     }
   end
 
+  def notify(:trader_state_updated, trader_state) do
+    GenServer.call(
+      :"#{__MODULE__}-#{trader_state.symbol}",
+      {:update_trader_state, trader_state}
+    )
+  end
+
+  def notify(:rebuy_triggered, trader_state) do
+    GenServer.call(
+      :"#{__MODULE__}-#{trader_state.symbol}",
+      {:rebuy_triggered, trader_state}
+    )
+  end
+
   def handle_continue(:start_traders, %{symbol: symbol} = state) do
     settings = fetch_symbol_settings(symbol)
-
-    trader_state = %Trader.State{
-      symbol: symbol,
-      profit_interval: settings.profit_interval,
-      tick_size: settings.tick_size
-    }
-
-    traders = for _i <- 1..settings.chunks, do: start_new_trader(trader_state)
+    trader_state = fresh_trader_state(symbol, settings)
+    traders = [start_new_trader(trader_state)]
 
     {:noreply, %{state | settings: settings, traders: traders}}
   end
@@ -67,6 +68,39 @@ defmodule Naive.Leader do
         {:reply, :ok, state}
 
       index ->
+        old_trader_data = Enum.at(traders, index)
+        new_trader_data = %{old_trader_data | state: new_trader_state}
+
+        {:reply, :ok, %{state | traders: List.replace_at(traders, index, new_trader_data)}}
+    end
+  end
+
+  def handle_call(
+        {:rebuy_triggered, new_trader_state},
+        {trader_pid, _},
+        %{traders: traders, symbol: symbol, settings: settings} = state
+      ) do
+    case Enum.find_index(traders, &(&1.pid == trader_pid)) do
+      nil ->
+        Logger.warn("Rebuy triggered by trader that leader is not aware of")
+        {:reply, :ok, state}
+
+      index ->
+        traders =
+          if settings.chunks == length(traders) do
+            Logger.info("All traders already started for #{symbol}")
+            traders
+          else
+            Logger.info("Starting new trader for #{symbol}")
+
+            new_trader =
+              symbol
+              |> fresh_trader_state(settings)
+              |> start_new_trader
+
+            [new_trader | traders]
+          end
+
         old_trader_data = Enum.at(traders, index)
         new_trader_data = %{old_trader_data | state: new_trader_state}
 
@@ -112,7 +146,26 @@ defmodule Naive.Leader do
     end
   end
 
-  def start_new_trader(state) do
+  defp fresh_trader_state(symbol, settings) do
+    budget_by_trader =
+      settings.budget
+      |> D.new()
+      |> D.div(D.new(settings.chunks))
+
+    %Trader.State{
+      id: System.system_time(),
+      symbol: symbol,
+      budget: budget_by_trader,
+      buy_down_interval: settings.buy_down_interval,
+      profit_interval: settings.profit_interval,
+      rebuy_interval: settings.rebuy_interval,
+      rebuy_notified: settings.rebuy_notified,
+      tick_size: settings.tick_size,
+      step_size: settings.step_size
+    }
+  end
+
+  defp start_new_trader(state) do
     {:ok, pid} =
       DynamicSupervisor.start_child(
         :"Naive.DynamicTraderSupervisor-#{state.symbol}",
@@ -125,23 +178,45 @@ defmodule Naive.Leader do
   end
 
   defp fetch_symbol_settings(symbol) do
-    tick_size = fetch_tick_size(symbol)
+    symbol_filters = fetch_symbol_filters(symbol)
 
-    %{
-      chunks: 1,
-      # -0.12% for quick testing
-      profit_interval: -0.0012,
-      tick_size: tick_size
-    }
+    Map.merge(
+      %{
+        chunks: 4,
+        budget: 20,
+        # 0.5%
+        buy_down_interval: 0.005,
+        # -0.12% for quick testing
+        profit_interval: -0.0012,
+        # 0.5%
+        rebuy_interval: 0.005,
+        rebuy_notified: false
+      },
+      symbol_filters
+    )
   end
 
-  defp fetch_tick_size(symbol) do
-    @binance_client.get_exchange_info()
-    |> elem(1)
-    |> Map.get(:symbols)
-    |> Enum.find(&(&1["symbol"] == String.upcase(symbol)))
-    |> Map.get("filters")
-    |> Enum.find(&(&1["filterType"] == @filter_type))
-    |> Map.get("tickSize")
+  defp fetch_symbol_filters(symbol) do
+    symbol_filters =
+      @binance_client.get_exchange_info()
+      |> elem(1)
+      |> Map.get(:symbols)
+      |> Enum.find(&(&1["symbol"] == symbol))
+      |> Map.get("filters")
+
+    tick_size =
+      symbol_filters
+      |> Enum.find(&(&1["filterType"] == "PRICE_FILTER"))
+      |> Map.get("tickSize")
+
+    step_size =
+      symbol_filters
+      |> Enum.find(&(&1["filterType"] == "LOT_SIZE"))
+      |> Map.get("stepSize")
+
+    %{
+      tick_size: tick_size,
+      step_size: step_size
+    }
   end
 end
--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -9,14 +9,30 @@ defmodule Naive.Trader do
   @binance_client Application.get_env(:naive, :binance_client)
 
   defmodule State do
-    @enforce_keys [:symbol, :profit_interval, :tick_size]
+    @enforce_keys [
+      :id,
+      :symbol,
+      :budget,
+      :buy_down_interval,
+      :profit_interval,
+      :rebuy_interval,
+      :rebuy_notified,
+      :tick_size,
+      :step_size
+    ]
 
     defstruct [
+      :id,
       :symbol,
+      :budget,
       :buy_order,
       :sell_order,
       :profit_interval,
-      :tick_size
+      :buy_down_interval,
+      :rebuy_interval,
+      :rebuy_notified,
+      :tick_size,
+      :step_size
     ]
   end
 
@@ -37,19 +53,40 @@ defmodule Naive.Trader do
     {:ok, state}
   end
 
+  @doc "Places a buy order"
   def handle_info(
         %TradeEvent{price: price},
-        %State{symbol: symbol, buy_order: nil} = state
+        %State{
+          id: id,
+          symbol: symbol,
+          budget: budget,
+          buy_order: nil,
+          buy_down_interval: buy_down_interval,
+          tick_size: tick_size,
+          step_size: step_size
+        } = state
       ) do
-    Logger.info("Placing buy order (#{symbol}@#{price})")
+    buy_price =
+      calculate_buy_price(
+        price,
+        buy_down_interval,
+        tick_size
+      )
 
-    quantity = 100
+    Logger.info("Trader(#{id}) placing buy order (#{symbol}@#{price})")
+
+    quantity =
+      calculate_quantity(
+        budget,
+        price,
+        step_size
+      )
 
     {:ok, %Binance.OrderResponse{} = order} =
       @binance_client.order_limit_buy(
         symbol,
         quantity,
-        price,
+        buy_price,
         "GTC"
       )
 
@@ -61,36 +98,69 @@ defmodule Naive.Trader do
   end
 
   def handle_info(
-        %TradeEvent{buyer_order_id: order_id, quantity: quantity},
+        %TradeEvent{buyer_order_id: order_id},
         %State{
           symbol: symbol,
           buy_order: %Binance.OrderResponse{
-            price: buy_price,
             order_id: order_id,
-            orig_qty: orig_qty
-          },
+            status: "FILLED"
+          }
+        } = state
+      ) do
+    {:no_reply, state}
+  end
+
+  @doc "Places a sell order"
+  def handle_info(
+        %TradeEvent{buyer_order_id: order_id},
+        %State{
+          id: id,
+          symbol: symbol,
+          buy_order:
+            %Binance.OrderResponse{
+              price: buy_price,
+              order_id: order_id,
+              orig_qty: quantity,
+              transact_time: timestamp
+            } = buy_order,
           profit_interval: profit_interval,
           tick_size: tick_size
         } = state
       ) do
-    sell_price =
-      calculate_sell_price(
-        buy_price,
-        profit_interval,
-        tick_size
-      )
-
-    Logger.info("Buy order filled, placing sell order (#{symbol}@#{sell_price})")
-
-    {:ok, %Binance.OrderResponse{} = order} =
-      @binance_client.order_limit_sell(
+    {:ok, %Binance.Order{} = current_buy_order} =
+      @binance_client.get_order(
         symbol,
-        quantity,
-        sell_price,
-        "GTC"
+        timestamp,
+        order_id
       )
 
-    new_state = %{state | sell_order: order}
+    buy_order = %{buy_order | status: current_buy_order.status}
+
+    {:ok, new_state} =
+      if buy_order.status == "FILLED" do
+        sell_price =
+          calculate_sell_price(
+            buy_price,
+            profit_interval,
+            tick_size
+          )
+
+        Logger.info(
+          "Trader(#{id}) buy order filled, placing sell order (#{symbol}@#{sell_price})"
+        )
+
+        {:ok, %Binance.OrderResponse{} = new_sell_order} =
+          @binance_client.order_limit_sell(
+            symbol,
+            quantity,
+            sell_price,
+            "GTC"
+          )
+
+        {:ok, %{state | buy_order: buy_order, sell_order: new_sell_order}}
+      else
+        {:ok, %{state | buy_order: buy_order}}
+      end
 
     Naive.Leader.notify(:trader_state_updated, new_state)
 
@@ -100,19 +170,99 @@ defmodule Naive.Trader do
   def handle_info(
         %TradeEvent{seller_order_id: order_id, quantity: quantity},
         %State{
-          sell_order: %Binance.OrderResponse{
-            order_id: order_id,
-            orig_qty: orig_qty
-          }
+          id: id,
+          symbol: symbol,
+          sell_order:
+            %Binance.OrderResponse{
+              order_id: order_id,
+              transact_time: timestamp
+            } = sell_order
         } = state
       ) do
-    Logger.info("Trade finished, trader will now exit")
+    {:ok, %Binance.Order{} = current_sell_order} =
+      @binance_client.get_order(
+        symbol,
+        timestamp,
+        order_id
+      )
 
-    {:stop, :normal, state}
+    sell_order = %{sell_order | status: current_sell_order.status}
+
+    if sell_order.status == "FILLED" do
+      Logger.info("Trader(#{id}) - Trade finished, trader will now exit")
+
+      {:stop, :trade_finished, state}
+    else
+      new_state = %{state | sell_order: sell_order}
+
+      {:noreply, new_state}
+    end
   end
 
-  def handle_info(%TradeEvent{} = event, state) do
+  def handle_info(
+        %TradeEvent{price: current_price} = event,
+        %State{
+          id: id,
+          symbol: symbol,
+          buy_order: %Binance.OrderResponse{
+            price: buy_price
+          },
+          rebuy_interval: rebuy_interval,
+          rebuy_notified: false
+        } = state
+      ) do
+    if trigger_rebuy?(buy_price, current_price, rebuy_interval) do
+      Logger.info("Rebuy triggered by trader(#{id}@#{symbol})")
+
+      new_state = %{state | rebuy_notified: true}
+
+      Naive.Leader.notify(:rebuy_triggered, new_state)
+
+      {:noreply, new_state}
+    else
+      {:noreply, state}
+    end
+  end
+
+  def handle_info(_, state) do
     {:noreply, state}
+  end
+
+  defp trigger_rebuy?(buy_price, current_price, rebuy_interval) do
+    current_price = float_to_decimal(current_price)
+    buy_price = float_to_decimal(buy_price)
+
+    rebuy_price =
+      D.sub(
+        buy_price,
+        D.mult(buy_price, float_to_decimal(rebuy_interval))
+      )
+
+    D.cmp(current_price, rebuy_price) == :lt
+  end
+
+  defp calculate_buy_price(price, buy_down_interval, tick_size) do
+    current_price = float_to_decimal(price)
+    interval = float_to_decimal(buy_down_interval)
+    tick = float_to_decimal(tick_size)
+    exact_buy_price = D.sub(current_price, D.mult(current_price, interval))
+
+    exact_buy_price
+    |> D.div_int(tick)
+    |> D.mult(tick)
+    |> D.to_float()
+  end
+
+  defp calculate_quantity(budget, price, step_size) do
+    step = float_to_decimal(step_size)
+    price = float_to_decimal(price)
+
+    exact_target_quantity = D.div(budget, price)
+
+    exact_target_quantity
+    |> D.div_int(step)
+    |> D.mult(step)
+    |> D.to_float()
   end
 
   defp calculate_sell_price(
@@ -142,5 +292,13 @@ defmodule Naive.Trader do
     |> D.div_int(tick)
     |> D.mult(tick)
     |> D.to_float()
+  end
+
+  defp float_to_decimal(float) when is_binary(float) do
+    float |> String.to_float |> D.from_float()
+  end
+
+  defp float_to_decimal(float) do
+    D.from_float(float)
   end
 end
