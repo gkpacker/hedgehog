65beaca23ff2ca537307b6df55c95b764c30f168
Gabriel <ggkpacker@gmail.com>
2021-01-23 13:11:38 -0300

    Create data warehouse to store trade events

---
+++ apps/data_warehouse/lib/data_warehouse/subscribers/worker.ex
@@ -0,0 +1,51 @@
+defmodule DataWarehouse.Subscribers.Worker do
+  use GenServer, restart: :temporary
+
+  require Logger
+
+  defmodule State do
+    @enforce_keys [:stream_name, :symbol]
+    defstruct [:stream_name, :symbol]
+  end
+
+  def start_link(%{stream_name: stream_name, symbol: symbol} = args) do
+    GenServer.start_link(
+      __MODULE__,
+      args,
+      name: :"#{__MODULE__}-#{stream_name}-#{symbol}"
+    )
+  end
+
+  def init(%{stream_name: stream_name, symbol: symbol}) do
+    topic = "#{stream_name}:#{symbol}"
+
+    Logger.info("DataWarehouse worker is subscribing to #{topic}")
+
+    Phoenix.PubSub.subscribe(
+      Streamer.PubSub,
+      topic
+    )
+
+    {:ok,
+     %State{
+       stream_name: stream_name,
+       symbol: symbol
+     }}
+  end
+
+  def handle_info(
+        %Streamer.Binance.TradeEvent{} = trade_event,
+        state
+      ) do
+    opts =
+      trade_event
+      |> Map.to_list()
+      |> Keyword.delete(:__struct__)
+
+    DataWarehouse.TradeEvent
+    |> struct!(opts)
+    |> DataWarehouse.Repo.insert!()
+
+    {:noreply, state}
+  end
+end
---
+++ apps/data_warehouse/mix.exs
@@ -0,0 +1,35 @@
+defmodule DataWarehouse.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :data_warehouse,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {DataWarehouse.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ecto_sql, "~> 3.5.4"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_pubsub, "~> 2.0"},
+      {:streamer, in_umbrella: true}
+    ]
+  end
+end
